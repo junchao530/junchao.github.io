fd4ebb5c8bbd3a5445b6719d5568436ddb8ff2a7
Jun <junchao530@gmail.com>
2023-09-19 15:31:13 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+
+}
+
+var tabButtons=document.querySelectorAll(".experience__container .button__container button");
+var tabPanels=document.querySelectorAll(".experience__container .tabPanel");
+
+function showPanel(panelIndex, colorCode){
+    
+
+    tabButtons.forEach(function(node){
+        node.style.backgroundColor="";
+        node.style.color="";
+    });
+    tabButtons[panelIndex].style.backgroundColor=colorCode;
+    tabButtons[panelIndex].style.color="white";
+    tabPanels.forEach(function(node){
+        node.style.display="none";
+    });
+    tabPanels[panelIndex].style.display="block";
+    tabPanels[panelIndex].style.backgroundColor=colorCode;
+}
+
+showPanel(0,'#373737')
